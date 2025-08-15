3e429611cbaa4d7228d7fd14e6b5e5efc09fb795
William Dodart <william.dodart@gmail.com>
2025-08-15 12:16:12 +0200

    feat(product-page) : add sections on the page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { ProductPage } from './pages/product-page/product-page';
 
 export const routes: Routes = [
-    { path : '', component : Home}
+    { path : '', component : Home},
+    { path : 'produits', component : ProductPage}
 ];
