4693f97bd4582f5f12694e2f0a1bfbbfb42d2a6e
William Dodart <william.dodart@gmail.com>
2025-08-19 15:47:09 +0200

    feat(app) : add product to cart directly on the card

--- src/app/components/product-component/product-component.ts
+++ src/app/components/product-component/product-component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ProductModel } from '../../models/product.model';
 import { Router } from '@angular/router';
 import { DecimalPipe } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-component',
@@ -13,11 +14,16 @@ export class ProductComponent {
 
   @Input() product!: ProductModel;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private cartService: CartService
+  ) {}
 
   public showProductPage(): void {
     this.router.navigateByUrl(`produits/${this.product.id}`)
   }
 
-
+  addToCart(event: MouseEvent): void {
+    event.stopPropagation();
+    this.cartService.addToCart(this.product);
+  }
 }
